c7c3d9ac2f98a750f1e5a7031a745df28d37caf9
raissa-eugenio <eugenio.r@aluno.ifsp.edu.br>
2025-05-06 17:13:14 -0300

    imagens adicionadas e modo de ampliação e fechamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,16 @@ document.addEventListener("DOMContentLoaded", function () {
         toggle.addEventListener('click', function () {
             const parent = this.closest('.dropdown-item');
             parent.classList.toggle('active');
-            
         })
     })
 
     //Fechat o dorpdonw ao clicar fora
-
     document.addEventListener('click', function (e) {
         dropdownToggle.forEach((toggle) => {
             const parent = toggle.closest('.dropdown-item');
             if (!parent.contains(e.target)) {
                 parent.classList.remove('active');
             }
-
         })
     })
 
@@ -25,24 +22,40 @@ document.addEventListener("DOMContentLoaded", function () {
 const searchToggle = document.getElementById('searchToggle');
 const searchOverlay = document.getElementById('searchOverlay');
 const closeSearch = document.getElementById('closeSearch');
-
 searchToggle.addEventListener('click', () => {
     searchOverlay.style.display = 'flex';
+
     setTimeout(() => {
         searchOverlay.querySelector('.search-input').focus();
     } , 100);
 });
 
 //fechar o botão
-
 closeSearch.addEventListener('click', () => {
     searchOverlay.style.display = 'none';
 })
 
 //fechar com o ESC
-
 document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
         searchOverlay.style.display = 'none';
     }
+})
+
+//Galeria
+const imagens = document.querySelectorAll('.galeria img');
+const lightbox = document.getElementById('lightbox');  
+const lightboxImg = document.getElementById('lightbox-img'); 
+
+imagens.forEach((img) => {
+    img.addEventListener('click', () => { 
+    lightboxImg.src = img.src;
+    lightbox.classList.add('show');
+   }) 
+})
+
+// esconde o lightbox
+
+lightbox.addEventListener('click', () => {
+    lightbox.classList.remove('show');
 })
