d2431a0998b0ac0dbe0d1671c429e5413f363945
raissa-eugenio <eugenio.r@aluno.ifsp.edu.br>
2025-05-20 15:56:55 -0300

    arrumando o login

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,3 +59,24 @@ imagens.forEach((img) => {
 lightbox.addEventListener('click', () => {
     lightbox.classList.remove('show');
 })
+
+//Abrir o login
+const openLoginBtn = document.getElementById('openLogin')
+const containerLogin = document.querySelector('.container-login')
+
+openLoginBtn.addEventListener('click', () => {
+    containerLogin.classList.add('active')
+})
+
+//Fechar ao clicar fora do formulario ou pressionar ESC
+containerLogin.addEventListener('click', (e) => {
+    if (e.target === containerLogin) {
+        containerLogin.classList.remove('active')
+    }
+})
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        containerLogin.classList.remove('active')
+    }
+})
